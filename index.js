9aecc802e99044ff701f85473918f15990be91fd
yasin al hasan <ya705668@gmail.com>
2024-06-02 17:52:12 +0600

    add new item food cart

--- index.js
+++ index.js
@@ -34,6 +34,7 @@ async function run() {
 
     const menuCollection = client.db("banglaDB").collection("menu");
     const reviewCollection = client.db("banglaDB").collection("reviews");
+    const cartCollection = client.db("banglaDB").collection("carts");
 
     app.get('/menu', async(req, res) => {
       const result = await menuCollection.find().toArray();
@@ -43,6 +44,14 @@ async function run() {
     app.get('/reviews', async(req, res) => {
       const result = await reviewCollection.find().toArray();
       res.send(result)
+    })
+
+
+    // carts collection
+    app.post('/carts', async(req, res) => {
+      const cartItem = req.body;
+      const result = await cartCollection.insertOne(cartItem);
+      res.send(result);
     })
 
 
