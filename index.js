2bc1a730cd121ee19aa008c9d344c0c24ae2ff00
yasin al hasan <ya705668@gmail.com>
2024-06-03 13:19:59 +0600

    add query for data collect

--- index.js
+++ index.js
@@ -55,7 +55,9 @@ async function run() {
     })
 
     app.get('/carts', async(req, res) => {
-      const result = await cartCollection.find().toArray();
+      const email = req.query.email;
+      const query = {email: email};
+      const result = await cartCollection.find(query).toArray();
       res.send(result)
     });
 
