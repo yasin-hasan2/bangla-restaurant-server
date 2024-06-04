d15a66a915aa8d6620b4434b465e5f3448556664
yasin al hasan <ya705668@gmail.com>
2024-06-05 00:01:44 +0600

    add deleted item

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3tdkdjy.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -48,6 +48,7 @@ async function run() {
 
 
     // carts collection
+    // ================================== USER DASHBOARD
     app.post('/carts', async(req, res) => {
       const cartItem = req.body;
       const result = await cartCollection.insertOne(cartItem);
@@ -60,6 +61,13 @@ async function run() {
       const result = await cartCollection.find(query).toArray();
       res.send(result)
     });
+
+    app.delete('/carts/:id', async(req, res) => {
+      const id = req.params.id
+      const query = {_id: new ObjectId(id)}
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
+    })
 
 
 
