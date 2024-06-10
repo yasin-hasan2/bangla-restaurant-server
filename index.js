f29985d1c2465d5d061a29e6fc1d3d81392a8dba
yasin al hasan <ya705668@gmail.com>
2024-06-10 17:14:50 +0600

    users data send and get and selet users rule

--- index.js
+++ index.js
@@ -39,6 +39,11 @@ async function run() {
 
 
     // users related api
+    app.get('/users', async(req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+
     app.post('/users', async (req, res) => {
       const user = req.body;
       // insert email if user doesn't exists;
@@ -53,9 +58,28 @@ async function run() {
     })
 
 
+    app.patch('/users/admin/:id', async(req, res) => {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)};
+      const updatedDoc = {
+        $set: {
+          role: 'admin'
+        }
+      }
+      const result = await userCollection.updateOne(filter, updatedDoc)
+      res.send(result)
+    })
+
+    app.delete('/users/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await userCollection.deleteOne(query);
+      res.send(result)
+    })
 
 
 
+    //menu related api
     app.get('/menu', async(req, res) => {
       const result = await menuCollection.find().toArray();
       res.send(result)
