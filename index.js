4d0ec9282eb7b8c6698c67cd22029ea577a56708
yasin al hasan <ya705668@gmail.com>
2024-07-02 10:29:01 +0600

    post admin-stats in server and also get that data from server

--- index.js
+++ index.js
@@ -261,6 +261,40 @@ async function run() {
     })
 
 
+    // stats or analytics 
+    app.get('/admin-stats', verifyToken, verifyAdmin, async(req, res) => {
+      const users = await userCollection.estimatedDocumentCount();
+      const menuItems = await menuCollection.estimatedDocumentCount();
+      const orders= await paymentCollection.estimatedDocumentCount();
+
+      // this is not the best way
+      // const payments = await paymentCollection.find().toArray();
+      // const revenue = payments.reduce((total, payment) => total + payment.price, 0);
+
+      // the better way
+      const result = await paymentCollection.aggregate([
+        {
+          $group: {
+            _id: null,
+            totalRevenue: {
+              $sum: '$price'
+            }
+          }
+        }
+      ]).toArray();
+
+      const revenue = result.length > 0 ? result[0].totalRevenue : 0;
+
+      res.send({
+        users,
+        menuItems,
+        orders,
+        revenue
+
+      })
+    })
+
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
